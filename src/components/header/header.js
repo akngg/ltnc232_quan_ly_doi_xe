046c20a788cb9cf9426fa73b3f726ddab391e98d
longhang2004 <long.hangnhut2004@hcmut.edu.vn>
2024-04-08 08:17:38 +0700

    abc

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,20 +3,6 @@ import { Outlet, Link } from "react-router-dom";
 import headerstyle from "./header.module.css"
 
 function Header(){
-    // return (
-    // <div className="thepage">
-    // <header className="headerbar">
-    // <img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/HCMCUT.svg" className="logoimg" alt="Logo Bách Khoa"></img>
-    //     <nav>
-    //         <Link to="/" className="thelink">Trang chủ</Link>
-    //         <Link to="/login" className="thelink">Đăng nhập</Link>
-    //         <Link to="/register" className="thelink">Đăng ký</Link>
-    //     </nav>
-    // </header>
-
-    // <Outlet className="therest"/>
-    // </div>
-    // )
     return (
         <div className={headerstyle.thepage}>
         <header className={headerstyle.headerbar}>
