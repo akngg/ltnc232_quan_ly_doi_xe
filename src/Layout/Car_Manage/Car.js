990aa0ff79117c2f9c01198b6cac9debca7d345f
ZiusTML <tml25092004@gmail.com>
2024-04-09 09:09:00 +0700

    fix

---
+++ src/Layout/Car_Manage/Car.js
@@ -0,0 +1,12 @@
+import './Car.css'
+const Car = () =>{
+    return (
+        <div className='Car'>
+            <h1 className='titleCar'>Car</h1>
+            <p>/Do something here...</p>
+        </div>
+    )
+    // TODO
+};
+
+export default Car;
---
+++ src/Layout/Driver_Manage/Driver.js
@@ -0,0 +1,11 @@
+import './Driver.css'
+const Driver = () =>{
+    return (
+        <div className='Driver'>
+            <h1 className='titleDriver'>Driver</h1>
+            <p>/Do something here...</p>
+        </div>
+    )
+};
+
+export default Driver;
