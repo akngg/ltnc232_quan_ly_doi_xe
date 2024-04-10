d81d731d794a56e1fcfbb61d74331abd6dfe5345
longhang2004 <long.hangnhut2004@hcmut.edu.vn>
2024-04-10 22:15:13 +0700

    af

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import './home.css'
-
+import { auth } from "../../modules/firebase";
 function Home(){
 return (<div className="homediv">
     <h1>Hệ thống quản lý đội xe</h1>
     <h3>By :</h3>
     <div className="logreg">
-    <Link to="/register" ><button className="homebtn">Đăng ký ngay</button></Link>
-    <Link to="/login" ><button className="homebtn">Đã có tài khoản?</button></Link>
+    <Link to={!auth.currentUser?"/register":"dashboard"} ><button className="homebtn">Đăng ký ngay</button></Link>
+    <Link to={!auth.currentUser?"/login":"/dashboard"} ><button className="homebtn">Đã có tài khoản?</button></Link>
     </div>
 </div>)
 } 
